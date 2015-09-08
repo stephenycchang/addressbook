0b3192cb8e888bb6a3f92c50e18e63add9c933ff
Stephen Chang <stephenycchang@gmail.com>
2015-09-08 16:17:06 -0400

    Created Address Book Model and declared association with User

---
+++ app/models/address_book.rb
@@ -0,0 +1,3 @@
+class AddressBook < ActiveRecord::Base
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908194442) do
+ActiveRecord::Schema.define(version: 20150908201526) do
+
+  create_table "address_books", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
