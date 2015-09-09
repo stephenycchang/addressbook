e595256ac58eabf00aaa8b7f0021882e95084980
Stephen Chang <stephenycchang@gmail.com>
2015-09-08 23:20:09 -0400

    Added Users controller as well as Bcrypt for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :address_books
   has_many :contacts, through :address_books 
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909030412) do
+ActiveRecord::Schema.define(version: 20150909031449) do
 
   create_table "address_books", force: :cascade do |t|
     t.string   "name"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20150909030412) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
